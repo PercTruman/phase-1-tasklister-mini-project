c7212471a13fc94bbfbf72b4e5fb7723b33c2da7
PercTruman <drshawnhart@gmail.com>
2022-04-10 13:47:24 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    makeNewTask(e.target.new_task_description.value)
+    })
 });
+
+
+ function makeNewTask(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'X'
+  btn.addEventListener('click', deleteTask)
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
+
+// function selectPriority(task){
+//   let prioritySelector = document.querySelector('select')
+//   console.log(prioritySelector)
+//   if (prioritySelector.target.red.value === 'red'){
+//     console.log('red')
+//   }
+// }
+
